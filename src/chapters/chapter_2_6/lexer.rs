528545684409c5a6f99e2f8a904345ddaeda61b7
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2023-03-08 18:12:13 +0900

    comment implemented

--- src/chapters/chapter_2_6/lexer.rs
+++ src/chapters/chapter_2_6/lexer.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use super::errors::LexerError;
 use super::*;
 
 pub struct Lexer {
@@ -62,7 +63,20 @@ impl Lexer {
             None => None,
         }
     }
-    pub fn scan(&mut self) -> Box<dyn Token> {
+
+    pub fn scan(&mut self) -> Result<Box<dyn Token>, LexerError> {
+        self.skip_whitespace();
+        match self.peek {
+            '/' => {
+                self.skip_comment()?;
+                self.scan()
+            }
+            _ if self.peek.is_digit(10) => Ok(Box::new(self.read_number()?)),
+            _ if self.peek.is_alphabetic() => Ok(Box::new(self.read_word()?)),
+            _ => Ok(Box::new(DefaultToken::new(self.peek))),
+        }
+    }
+    fn skip_whitespace(&mut self) {
         loop {
             let ch = self.read();
 
@@ -82,47 +96,88 @@ impl Lexer {
                 }
             }
         }
-        match self.peek {
-            _ if self.peek.is_digit(10) => {
-                let mut v: u32 = 0;
-                loop {
-                    v = 10 * v + self.peek.to_digit(10).unwrap();
-                    let ch = self.read();
-                    match ch {
-                        Some(ch) if ch.is_digit(10) => {
-                            self.peek = ch;
-                        }
-                        _ => {
-                            break;
-                        }
-                    }
+    }
+    fn read_number(&mut self) -> Result<Num, LexerError> {
+        let mut v: u32 = 0;
+        loop {
+            v = 10 * v + self.peek.to_digit(10).unwrap();
+            let ch = self.read();
+            match ch {
+                Some(ch) if ch.is_digit(10) => {
+                    self.peek = ch;
                 }
-                Box::new(Num::new(v))
+                Some(ch) if ch.is_whitespace() => {
+                    break;
+                }
+                None => {
+                    break;
+                }
+
+                _ => return Err(LexerError::UnexpectedEOF),
             }
-            _ if self.peek.is_alphabetic() => {
-                let mut b = String::new();
-                loop {
-                    b.push(self.peek);
-                    let ch = self.read();
-                    match ch {
-                        Some(ch) if ch.is_alphanumeric() => {
-                            self.peek = ch;
-                        }
-                        _ => {
-                            break;
-                        }
-                    }
+        }
+        Ok(Num::new(v))
+    }
+    fn read_word(&mut self) -> Result<Word, LexerError> {
+        let mut b = String::new();
+        loop {
+            b.push(self.peek);
+            let ch = self.read();
+            match ch {
+                Some(ch) if ch.is_alphanumeric() => {
+                    self.peek = ch;
                 }
-                let w = self.words.get(&b);
-                if let Some(w) = w {
-                    Box::new(w.clone())
-                } else {
-                    let w = Word::new(Tag::ID, b);
-                    self.words.insert(w.lexeme.clone(), w.clone());
-                    Box::new(w)
+                _ => {
+                    break;
                 }
             }
-            _ => Box::new(DefaultToken::new(self.peek)),
+        }
+        let w = self.words.get(&b);
+        if let Some(w) = w {
+            Ok(w.clone())
+        } else {
+            let w = Word::new(Tag::ID, b);
+            self.words.insert(w.lexeme.clone(), w.clone());
+            Ok(w)
+        }
+    }
+    fn skip_comment(&mut self) -> Result<(), LexerError> {
+        let ch = self.read();
+        match ch {
+            Some('/') => loop {
+                let ch = self.read();
+                match ch {
+                    Some('\n') => {
+                        self.line += 1;
+                        break Ok(());
+                    }
+                    None => break (Err(LexerError::UnexpectedEOF)),
+                    _ => continue,
+                }
+            },
+            Some('*') => loop {
+                let ch = self.read();
+                match ch {
+                    Some('\n') => {
+                        self.line += 1;
+                    }
+                    Some('*') => {
+                        let ch = self.read();
+                        match ch {
+                            Some('/') => break Ok(()),
+                            None => break Err(LexerError::UnexpectedEOF),
+                            _ => continue,
+                        }
+                    }
+                    None => {
+                        break Err(LexerError::UnexpectedEOF);
+                    }
+                    _ => {
+                        continue;
+                    }
+                }
+            },
+            _ => Ok(()),
         }
     }
 }
@@ -149,9 +204,64 @@ mod lexer_tests {
     fn test_first_scan() {
         let mut lexer = Lexer::new("a = 1".to_string());
         let token = lexer.scan();
+        assert!(token.is_ok());
         assert_eq!(
-            token.to_string(),
+            token.unwrap().to_string(),
             Word::new(Tag::ID, "a".to_string()).to_string()
         );
+    }
+    #[test]
+    fn test_white_space() {
+        let mut lexer = Lexer::new("\t \n a = 1".to_string());
+        let token = lexer.scan();
+        assert!(token.is_ok());
+        assert_eq!(
+            token.unwrap().to_string(),
+            Word::new(Tag::ID, "a".to_string()).to_string()
+        );
+    }
+    #[test]
+    fn test_single_line_comment() {
+        let mut lexer = Lexer::new("//this should be passed\na = 1".to_string());
+        let token = lexer.scan();
+        assert!(token.is_ok());
+        assert_eq!(
+            token.unwrap().to_string(),
+            Word::new(Tag::ID, "a".to_string()).to_string()
+        );
+    }
+    #[test]
+    fn test_single_line_comment_fail() {
+        let mut lexer = Lexer::new("//this should not be passed".to_string());
+        let token = lexer.scan();
+        assert!(token.is_err());
+        assert_eq!(token.err(), Some(LexerError::UnexpectedEOF),);
+    }
+    #[test]
+    fn test_multiline_comment() {
+        let mut lexer = Lexer::new("/* this should be passed */a=1".to_string());
+        let token = lexer.scan();
+        assert!(token.is_ok());
+        assert_eq!(
+            token.unwrap().to_string(),
+            Word::new(Tag::ID, "a".to_string()).to_string()
+        );
+    }
+    #[test]
+    fn test_multiline_comment_asterisk() {
+        let mut lexer = Lexer::new("/* this should be **** passed */a=1".to_string());
+        let token = lexer.scan();
+        assert!(token.is_ok());
+        assert_eq!(
+            token.unwrap().to_string(),
+            Word::new(Tag::ID, "a".to_string()).to_string()
+        );
+    }
+    #[test]
+    fn test_multiline_comment_EOF() {
+        let mut lexer = Lexer::new("/* this should not **** pass".to_string());
+        let token = lexer.scan();
+        assert!(token.is_err());
+        assert_eq!(token.err(), Some(LexerError::UnexpectedEOF),);
     }
 }
--- src/chapters/chapter_2_6/mod.rs
+++ src/chapters/chapter_2_6/mod.rs
@@ -1,3 +1,4 @@
+mod errors;
 mod lexer;
 mod num;
 mod tag;
